2b809564c83f402a8cb00667bf8e8e3d8ee39aa0
alexandrepping <alexandrefeevale@gmail.com>
2020-08-19 22:38:53 -0300

    criado o componentr Header e realizado comunicação com o App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import Header from "./components/Header";
 
+/*
+componente
+propriedade
+estado
+*/
+
 function App() {
+  //FRAGMENT
   return (
-    <div>
-      <Header />
-      <Header />
-    </div>
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
   );
 }
 
